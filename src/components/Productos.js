d6e82641fde494265602dcb8f351dbc0309c35c3
JLemus89 <johnlemus@hotmail.es>
2022-10-22 23:36:42 -0500

    ajuste catalogo productos (imagenes, nombre, descripcion, etc)

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -13,18 +13,19 @@ const Productos = () => {
                 <div className="card">
                     <div className="card-body">
                         <img src={item.urlImagen} width="100" className="my-4 mx-auto d-block"/>
-                        <h5 className="card-title">{item.nombre}</h5>
-                        <p className="card-text">{item.descripcion}</p>
-                        <p claclassNamess="card-text">{item.características}</p>
-                        <p claclassNamess="card-text">{item.precio}</p>
-                        <label for="cantidad" className="form-label">Cantidad</label>
+                        <h5 className="card-title">nombre: {item.nombre}</h5>
+                        <p className="card-text">descripcion: {item.descripcion}</p>
+                        <p claclassNamess="card-text">caracteristicas: {item.características}</p>
+                        <p claclassNamess="card-text">precio : $ {item.precio}</p>
+                       
+                    </div>
+                    <label for="cantidad" className="form-label">Cantidad</label>
                         <div className="inp_cantidad">
                         <input  type="number"  className="card-cantidad" /><br/>
                         <button type="button" className="btn btn-secondary">Agregar</button>
                         </div>
-                        
-                    </div>
                 </div>
+                
             ))}
         </div>
     </div>
--- src/data/bdProductos.js
+++ src/data/bdProductos.js
@@ -1,81 +1,65 @@
 const jsonProductos = [
     {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+      "id": 1,
+      "urlImagen": "https://exitocol.vtexassets.com/arquivos/ids/11463148-500-auto?v=1752884775&width=500&height=auto&aspect=true",
+      "nombre": "Jabon Rey",
+      "descripcion": "Jabón azul en barra x3 unidades",
       "características": "▪️ 8 Android stickers\n ▪️ White colored sticker sheet",
-      "precio": "$ 2.99"
+      "precio": 2350
     },
     {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+      "id": 2,
+      "urlImagen": "https://exitocol.vtexassets.com/arquivos/ids/15022152-500-auto?v=1752884516&width=500&height=auto&aspect=true",
+      "nombre": "Detergente FAB en polvo",
+      "descripcion": "Detergente en polvo multi uso",
+      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
+      "precio": 23350
+    },
+    {
+      "id": 3,
+      "urlImagen": "https://exitocol.vtexassets.com/arquivos/ids/13315271-500-auto?v=1752889218&width=500&height=auto&aspect=true",
+      "nombre": "Clorox",
+      "descripcion": "Blanqueador ropa aroma floral",
+      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
+      "precio": 16050
+    },
+    {
+      "id": 4,
+      "urlImagen": "https://exitocol.vtexassets.com/arquivos/ids/11455567-500-auto?v=1752893488&width=500&height=auto&aspect=true",
+      "nombre": "Esponja eterna",
+      "descripcion": "Esponja doble uso",
+      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
+      "precio": 4760
+    },
+    {
+      "id": 5,
+      "urlImagen": "https://stockimages.tiendasd1.com/stockimages.tiendasd1.com/kobastockimages/IMAGENES/12001316.png",
+      "nombre": "Bonaropa",
+      "descripcion": "Detergente liquido para ropa oscura",
+      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
+      "precio": 6450
+    },
+    {
+      "id": 6,
+      "urlImagen": "https://stockimages.tiendasd1.com/stockimages.tiendasd1.com/kobastockimages/IMAGENES/12000130.png",
+      "nombre": "Polvo abrasivo brilla king",
+      "descripcion": "polvo abrasivo para limpieza",
+      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
+      "precio": 2150
+    },
+    {
+      "id": 7,
+      "urlImagen": "https://stockimages.tiendasd1.com/stockimages.tiendasd1.com/kobastockimages/IMAGENES/12000232.png",
+      "nombre": "Limpia vidrios brilla king",
+      "descripcion": "Liquido limpia vidrios brilla king",
       "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
       "precio": "$ 2.99"
     },
     {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
-      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
-      "precio": "$ 2.99"
-    },
-    {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
-      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
-      "precio": "$ 2.99"
-    },
-    {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
-      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
-      "precio": "$ 2.99"
-    },
-    {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
-      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
-      "precio": "$ 2.99"
-    },
-    {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
-      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
-      "precio": "$ 2.99"
-    },
-    {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
-      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
-      "precio": "$ 2.99"
-    },
-    {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
-      "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
-      "precio": "$ 2.99"
-    },
-    {
-      "id": "GGOEAFKA087499",
-      "urlImagen": "https://i.blogs.es/27b569/telefono/450_1000.jpeg",
-      "nombre": "Android Small Removable Sticker Sheet",
-      "descripcion": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+      "id": 8,
+      "urlImagen": "https://stockimages.tiendasd1.com/stockimages.tiendasd1.com/kobastockimages/IMAGENES/12000231.png",
+      "nombre": "Lavaloza brilla king",
+      "descripcion": "lavaloza liquido brilla king",
       "características": "▪️ 8 Android stickers\n▪️ White colored sticker sheet",
       "precio": "$ 2.99"
     }
