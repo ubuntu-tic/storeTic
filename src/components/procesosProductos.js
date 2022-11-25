70bb028745f7bb727d473ffc5f25118385e8a418
JLemus89 <johnlemus@hotmail.es>
2022-11-25 07:51:38 -0500

    make it better

--- src/components/procesosProductos.js
+++ src/components/procesosProductos.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 export const getProducts = async () => {
   const response = await axios.get(
-    `http://localhost:3000/productos`
+    `https://storetic-app.herokuapp.com/productos`
   );
   return response.data;
 }
 
 export const getProductByID = async (_id) => {
   const response = await axios.get(
-    `http://localhost:3000/productos/${_id}`
+    `https://storetic-app.herokuapp.com/productos/${_id}`
   );
   return response.data;
 }
 
 export const addProduct = async (product) => {
   const response = await axios.post(
-    `http://localhost:3000/productos`,
+    `https://storetic-app.herokuapp.com/productos`,
     product
   );
   return response.data;
@@ -24,7 +24,7 @@ export const addProduct = async (product) => {
 
 export const updateProduct = async (_id, product) => {
   const response = await axios.put(
-    `http://localhost:3000/productos/${_id}`,
+    `https://storetic-app.herokuapp.com/productos/${_id}`,
     product
   );
   return response.data;
@@ -32,7 +32,7 @@ export const updateProduct = async (_id, product) => {
 
 export const deleteProduct = async (_id) => {
   const response = await axios.delete(
-    `http://localhost:3000/productos/${_id}`
+    `https://storetic-app.herokuapp.com/productos/${_id}`
   );
   return response.data;
 }
--- src/components/procesosUsuarios.js
+++ src/components/procesosUsuarios.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export const getUsuarios = async () => {
   const response = await axios.get(
-    `http://localhost:3000/usuarios`
+    `https://storetic-app.herokuapp.com/usuarios`
   );
   return response.data;
 }
 
 export const getUsuarioById = async (_id) => {
   const response = await axios.get(
-    `http://localhost:3000/usuarios/${_id}`
+    `https://storetic-app.herokuapp.com/usuarios/${_id}`
   );
   return response.data;
 }
