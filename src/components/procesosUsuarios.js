36e39bf2a65cf0a5cc2ee5db0b834d24a36213f6
JLemus89 <johnlemus@hotmail.es>
2022-11-25 20:12:27 -0500

    make it better

--- src/components/procesosUsuarios.js
+++ src/components/procesosUsuarios.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export const getUsuarios = async () => {
   const response = await axios.get(
-    `http://localhost:3000/usuarios`
+    `/usuarios`
   );
   return response.data;
 }
 
 export const getUsuarioById = async (_id) => {
   const response = await axios.get(
-    `http://localhost:3000/usuarios/${_id}`
+    `/usuarios/${_id}`
   );
   return response.data;
 }
--- src/components/procesosVentas.js
+++ src/components/procesosVentas.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export const getVentas = async () => {
   const response = await axios.get(
-    `http://localhost:3000/ventas`
+    `/ventas`
   );
   return response.data;
 }
 
 export const getVentaById = async (_id) => {
   const response = await axios.get(
-    `http://localhost:3000/ventas/${_id}`
+    `/ventas/${_id}`
   );
   return response.data;
 }
@@ -17,7 +17,7 @@ export const getVentaById = async (_id) => {
 
 export const confirmarCompra = async (venta) => {
   const response = await axios.post(
-    `http://localhost:3000/ventas`,
+    `/ventas`,
     venta
   );
   return response.data;
